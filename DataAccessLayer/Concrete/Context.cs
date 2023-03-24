f6c915c4cb9f514ef129d24e20dd44ef37320f0a
hüseyin cangür <hcangur3@gmail.comn>
2023-03-25 01:16:14 +0300

    Context oluşturuldu

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=DESKTOP-10S04G1\SQLEXPRESS database=TraversalDB; integrated security=true");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+       
+
+
+    }
+}
